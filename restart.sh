6e229a4f023c7d172b78fda52f6faba42fce7bf9
JeyJey871 <ginza@tuit.uz>
2024-03-04 03:34:59 +0500

    emaktab 100

--- restart.sh
+++ restart.sh
@@ -11,5 +11,13 @@ sudo systemctl restart djangofather.service
 sudo systemctl restart djangofather_beat
 sudo systemctl restart djangofather_worker
 
+
+#status
+sudo systemctl status djangofather.socket
+sudo systemctl status djangofather.service
+sudo systemctl status djangofather_beat
+sudo systemctl status djangofather_worker
+
+
 sudo systemctl daemon-reload
 sudo nginx -t && sudo systemctl restart nginx
