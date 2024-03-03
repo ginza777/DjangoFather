0e1370986c01be926493c063724a1574e6c02699
JeyJey871 <ginza@tuit.uz>
2024-03-04 03:34:21 +0500

    emaktab 100

--- restart.sh
+++ restart.sh
@@ -5,8 +5,7 @@
 # Systemd xizmatlarini qayta boshlash
 
 git pull
-sudo systemctl start djangofather.socket
-sudo systemctl enable djangofather.socket
+
 sudo systemctl restart djangofather.socket
 sudo systemctl restart djangofather.service
 sudo systemctl restart djangofather_beat
