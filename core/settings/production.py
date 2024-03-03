999fe1f631d7d5c8a97cd472e9e9280a1aee8242
JeyJey871 <ginza@tuit.uz>
2024-03-03 07:21:13 +0500

    common

--- core/settings/production.py
+++ core/settings/production.py
@@ -15,8 +15,10 @@ IF YOU WANT SET CSRF_TRUSTED_ORIGINS = ["*"] THEN YOU SHOULD SET:
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 """
 
+
+
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["https://crm.sherzamon.cloud"]
+CSRF_TRUSTED_ORIGINS = ["https://crm.sherzamon.cloud","http://crm.sherzamon.cloud"]
 
 ###################################################################
 # CORS
@@ -25,6 +27,8 @@ CSRF_TRUSTED_ORIGINS = ["https://crm.sherzamon.cloud"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
+CSRF_COOKIE_HTTPONLY = True
+
 
 
 
