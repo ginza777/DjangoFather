31b6dd3754e2381356179f450a6289f7364a7b62
JeyJey871 <ginza@tuit.uz>
2024-03-31 21:26:17 +0500

    new app

--- projects/uzummarket/admin.py
+++ projects/uzummarket/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Shop, Vendor, Product, Order
 # Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['nomi', 'manzili', 'telefon', 'extra', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['nomi', 'manzili', 'telefon']
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['nomi', 'rasmi']
+    search_fields = ['nomi']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['vendor', 'shop', 'product', 'count', 'price', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['vendor', 'shop', 'product']
