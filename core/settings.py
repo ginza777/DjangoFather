235f4c39bb078bbc9d9591038d5674254f2a0118
JeyJey871 <ginza@tuit.uz>
2024-04-01 18:46:22 +0500

    new app

--- core/settings.py
+++ core/settings.py
@@ -169,11 +169,12 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 # MODEL TRANSLATION CONFIG
 gettext = lambda s: s  # noqa
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 # CACHES
