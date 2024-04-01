ac808ed2ee7ce96bdaf605b438bebaa1e7308182
JeyJey871 <ginza@tuit.uz>
2024-04-01 17:54:28 +0500

    new app

--- projects/uzummarket/admin.py
+++ projects/uzummarket/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Shop, Vendor, Product, Order
+from django.utils.html import format_html
 # Register your models here.
 
 @admin.register(Shop)
@@ -15,8 +16,13 @@ class VendorAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['nomi', 'rasmi']
+    list_display = ['nomi', 'image_html']
     search_fields = ['nomi']
+
+    def image_html(self, obj):
+        if obj.rasmi:
+            return format_html(f'<img src="{obj.rasmi.url}" width="100" height="100" />')
+        return None
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
